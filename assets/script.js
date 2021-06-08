05035d250ba2a178667abd68666a42d42fdfb654
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-06-08 11:52:27 +0000

    script.js- experimenting with js calculations

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+
+
+var taxBreak = 12570;
+var natIns = 9500;
+var basicRate = 12571;
+var higherRate = 50270;
+var additionalRate = 150001;
+
+function calcNet(amount){
+    (document.getElementById('grossIncome'));
+    if (amount > additionalRate){
+        net =grossIncome -(((amount-additionalRate)*.45) + ((amount-higherRate)*.40) + ((amount-basicRate)*.20) + ((amount-natIns)*.12));
+    } else if (amount < additionalRate &&  amount >= higherRate){
+        net =grossIncome -(((amount-higherRate)*.40) + ((amount-basicRate)*.20) + ((amount-natIns)*.12));
+    } else if (amount < higherRate && amount >= basicRate){
+        net =grossIncome -( ((amount-basicRate)*.20) + ((amount-natIns)*.12));
+    } else if (amount < basicRate && amount >= natIns){
+        net = grossIncome -(((amount-natIns)*.12));
+    } else if (amount < natIns){
+        net = grossIncome;
+    }
+
+}
