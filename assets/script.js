8cc9003f31ce028afef90e67be6e2386cd43d67a
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-06-09 11:11:57 +0000

    script.js - added variables

--- assets/script.js
+++ assets/script.js
@@ -6,14 +6,16 @@ var basicRate = 12571;
 var higherRate = 50270;
 var additionalRate = 150001;
 
+var net;
+
 function calcNet(amount){
     (document.getElementById('grossIncome'));
     if (amount > additionalRate){
-        net =grossIncome -(((amount-additionalRate)*.45) + ((amount-higherRate)*.40) + ((amount-basicRate)*.20) + ((amount-natIns)*.12));
+        net = grossIncome -(((amount-additionalRate)*.45) + ((amount-higherRate)*.40) + ((amount-basicRate)*.20) + ((amount-natIns)*.12));
     } else if (amount < additionalRate &&  amount >= higherRate){
-        net =grossIncome -(((amount-higherRate)*.40) + ((amount-basicRate)*.20) + ((amount-natIns)*.12));
+        net = grossIncome -(((amount-higherRate)*.40) + ((amount-basicRate)*.20) + ((amount-natIns)*.12));
     } else if (amount < higherRate && amount >= basicRate){
-        net =grossIncome -( ((amount-basicRate)*.20) + ((amount-natIns)*.12));
+        net = grossIncome -( ((amount-basicRate)*.20) + ((amount-natIns)*.12));
     } else if (amount < basicRate && amount >= natIns){
         net = grossIncome -(((amount-natIns)*.12));
     } else if (amount < natIns){
