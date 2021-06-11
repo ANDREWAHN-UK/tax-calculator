17b7ebe7952860e444850095e2a553b6e7878e3f
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-06-11 11:08:41 +0000

    script.js - added code for button clicking in section 3

--- assets/script.js
+++ assets/script.js
@@ -47,3 +47,11 @@ function myFunction(num) {
     document.getElementById('natInsOwed').innerHTML = natInsPaid;
     document.getElementById('netWage').innerHTML = net;
 }
+
+// click functionality for section 3
+document.querySelectorAll('.story-btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+        btn.classList.toggle('change')
+        btn.nextElementSibling.classList.toggle('change')
+    })
+    })
