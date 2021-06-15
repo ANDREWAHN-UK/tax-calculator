7ed96822504e29a3410c388c8fed27345d458ff3
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-06-15 15:38:09 +0000

    added js code to determine student loan amounts, updated formula for net calculations

--- assets/script.js
+++ assets/script.js
@@ -1,53 +1,88 @@
+function calcNet(num) {
+    let gross = document.getElementById("grossIncome").value;
+    let taxBreak = 12570;
+    let natIns = 9500;
+    let natInsHigher = 50270;
+    let basicRate = 12571;
+    let higherRate = 50270;
+    let additionalRate = 150001;
+    let maxNormalTax = 7539.8; //because tax works in bands, this is the highest level at 20% tax possible.
+    let maxNormalInsurance = 4892.4; //same as previous, this is the max normal rate national insurance
 
-    
-    function myFunction(num) {
-    var gross = document.getElementById("grossIncome").value;
-    var taxBreak = 12570;
-    var natIns = 9500;
-    var natInsHigher = 50270;
-    var basicRate = 12571;
-    var higherRate = 50270;
-    var additionalRate = 150001;
-    var maxNormalTax = 7539.8; //because tax works in bands, this is the highest level at 20% tax possible.
-    var maxNormalInsurance = 4892.4; //same as previous, this is the max normal rate national insurance
-    var net;
-    var taxPaid;
-    var natInsPaid;
+    let studentLoanChoice = document.getElementById("student-loan").value;
+    let studentLoanOne = 19895;
+    let studentLoanTwo = 27295;
+    let studentLoanFour = 25000;
+    let studentLoanPaid;
 
-    if (gross >= additionalRate){
-        natInsPaid =((gross - natInsHigher) * .02) + maxNormalInsurance;
-        taxPaid = ((gross -additionalRate) *.45) + ((gross - higherRate) * .40) + maxNormalTax;
-        net = (gross -taxPaid -natInsPaid);
-        console.log (natInsPaid, taxPaid, net, gross);
+    let net;
+    let taxPaid;
+    let natInsPaid;
+
+
+    switch (studentLoanChoice) {
+        case '0':
+            studentLoanPaid =  0;
+            console.log('choice 0', studentLoanPaid);
+            break
+        case '1':
+            studentLoanPaid = ((gross - studentLoanOne)*0.09);
+            console.log('choice 1', studentLoanPaid);
+            break
+        case '2':
+            studentLoanPaid =  ((gross - studentLoanTwo)*0.09);
+            console.log('choice 2', studentLoanPaid);
+            break
+        case '4':
+            studentLoanPaid = ((gross - studentLoanFour)*0.09)
+            console.log('choice 4', studentLoanPaid);
+            break
     }
-    else if(gross >= higherRate && gross < additionalRate) {        
+ 
+
+
+    if (gross >= additionalRate) {
+        natInsPaid = ((gross - natInsHigher) * .02) + maxNormalInsurance;
+        taxPaid = ((gross - additionalRate) * .45) + ((gross - higherRate) * .40) + maxNormalTax;
+        net = (gross - taxPaid - natInsPaid -studentLoanPaid);
+
+    } else if (gross >= higherRate && gross < additionalRate) {
         natInsPaid = ((gross - natInsHigher) * .02) + maxNormalInsurance;
         taxPaid = ((gross - higherRate) * .40) + maxNormalTax;
-        net = (gross -taxPaid -natInsPaid);
-        console.log (natInsPaid, taxPaid, net, gross);
-        
-    } else if (gross >= basicRate && gross < higherRate) {
+        net = (gross - taxPaid - natInsPaid -studentLoanPaid);
+
+
+    } else if (gross >= basicRate && gross < higherRate && gross >=studentLoanTwo) {
         natInsPaid = ((gross - natIns) * .12);
         taxPaid = ((gross - taxBreak) * .2);
-        net = (gross - taxPaid - natInsPaid);
-        console.log (natInsPaid, taxPaid, net, gross);
+        net = (gross - taxPaid - natInsPaid - studentLoanPaid);
+
+    } else if (gross >= basicRate && gross < higherRate ) {
+        natInsPaid = ((gross - natIns) * .12);
+        taxPaid = ((gross - taxBreak) * .2);
+        net = (gross - taxPaid - natInsPaid - studentLoanPaid);
+
 
     } else if (gross >= natIns) {
         natInsPaid = ((gross - natIns) * .12);
-        taxPaid=0;
-        net = (gross - natInsPaid);
-        console.log (natInsPaid, taxPaid, net, gross);
+        taxPaid = 0;
+        studentLoanPaid =0;
+        net = (gross - natInsPaid -studentLoanPaid );
+
 
     } else if (gross < natIns) {
         net = gross;
         natInsPaid = 0;
-        taxPaid= 0;
-        console.log (natInsPaid, taxPaid, net, gross);
+        taxPaid = 0;
     }
+
     document.getElementById('grossIncomeEntered').innerHTML = '£ ' + gross;
     document.getElementById('taxOwed').innerHTML = '£ ' + taxPaid;
     document.getElementById('natInsOwed').innerHTML = '£ ' + natInsPaid;
     document.getElementById('netWage').innerHTML = '£ ' + net;
+    document.getElementById('studentLoanOwed').innerHTML = '£ ' + studentLoanPaid;
+
+
 }
 
 // click functionality for section 3
@@ -56,4 +91,4 @@ document.querySelectorAll('.story-btn').forEach(btn => {
         btn.classList.toggle('change')
         btn.nextElementSibling.classList.toggle('change')
     })
-    })
+})
