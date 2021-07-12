fe0ba2aaec632fef3636891dab93cbf0455de83f
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-07-12 09:29:43 +0000

    index,style,script - renoved swipe js code as it wasn't working

--- assets/script.js
+++ assets/script.js
@@ -230,16 +230,3 @@ window.onclick = function (event) {
     }
 }
 
-// swipe.js settings
-var element = document.getElementById('mySwipe');
-window.mySwipe = new Swipe(element, {
-  startSlide: 0,
-  auto: 3000,
-  draggable: false,
-  autoRestart: false,
-  continuous: true,
-  disableScroll: true,
-  stopPropagation: true,
-  callback: function(index, element) {},
-  transitionEnd: function(index, element) {}
-});
