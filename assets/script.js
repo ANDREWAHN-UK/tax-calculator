42de8b2aee5468661389fd1cf8431cd325f0e4eb
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-07-12 09:21:16 +0000

    script.js - added javascript for swipe.js

--- assets/script.js
+++ assets/script.js
@@ -229,3 +229,17 @@ window.onclick = function (event) {
         modal3.style.display = "none";
     }
 }
+
+// swipe.js settings
+var element = document.getElementById('mySwipe');
+window.mySwipe = new Swipe(element, {
+  startSlide: 0,
+  auto: 3000,
+  draggable: false,
+  autoRestart: false,
+  continuous: true,
+  disableScroll: true,
+  stopPropagation: true,
+  callback: function(index, element) {},
+  transitionEnd: function(index, element) {}
+});
