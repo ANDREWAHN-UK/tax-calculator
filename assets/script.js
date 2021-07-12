0fecc8726f0bad7d03ff702d1826d6975f9faad4
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-07-12 09:51:11 +0000

    index,style,script - removed swiper.js elements

--- assets/script.js
+++ assets/script.js
@@ -229,25 +229,3 @@ window.onclick = function (event) {
         modal3.style.display = "none";
     }
 }
-
-const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
